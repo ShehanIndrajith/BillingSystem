7c8b3600b9c80999aaef0af6b660fb7bcb283a12
Shehan Indrajith <sbsindrajith2002@gmail.com>
2025-11-08 17:07:45 +0530

    Commit Testing Changes and references

---
+++ BillingSystem.Test/SampleTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace BillingSystem.Tests
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void TestExample()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
